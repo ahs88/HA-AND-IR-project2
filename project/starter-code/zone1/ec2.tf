53be7762859386a3f0c853513c762dd5a05f5062
Akshath-Shetty <Akshath.Shetty@ibm.com>
2023-02-15 21:30:30 +0530

    ami change

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0ae9dae06083ba4d9"
+   aws_ami            = "ami-008917d0e77fdf007"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
